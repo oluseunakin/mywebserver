e51edec818acc36692b21d393dace95581132e4e
oluseunakin <akindoyinoluseun@gmail.com>
2023-01-27 15:36:21 +0100

    third

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-import http from "http"
+import http from "http";
 
 const server = http.createServer((req, res) => {
-    
-    res.writeHead(200).end(req.url)
-})
+  if (req.method === "GET") {
+    res.writeHead(200).end(req.url);
+  }else res.writeHead(200).end("sent")
+});
 
 server.listen(() => {
-    console.log("server started")
-})
+  console.log("server started");
+});
 
-server.on('error', e => {
-    console.log(e)
-})
+server.on("error", (e) => {
+  console.log(e);
+});
