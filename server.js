38e10d0baf6dfc81b6f3012c0c8ed633a5385b3a
oluseunakin <akindoyinoluseun@gmail.com>
2023-01-27 17:08:02 +0100

    jhjhjhj

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  if (req.method === "GET") {
-    res.writeHead(200).end(req.url);
-  }else res.writeHead(200).end("sent")
+  res.end('bye')
 });
 
 server.listen(() => {
@@ -11,5 +9,5 @@ server.listen(() => {
 });
 
 server.on("error", (e) => {
-  console.log(e);
+  console.log(e.message);
 });
